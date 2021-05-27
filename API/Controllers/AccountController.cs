00d6bf5a70b7f1d1a1a576df4c9593c0d732c010
Hamdi Çatal <hamdicatal@outlook.com>
2021-05-27 14:38:51 +0300

    Kullanıcı girişi için login metodu eklendi.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -22,18 +22,38 @@ namespace API.Controllers
         {
             if (await UserExists(request.Username))
                 return BadRequest("Username is taken before!");
-                
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
                 UserName = request.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Username)),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
                 PasswordSalt = hmac.Key
             };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO request)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == request.Username);
+
+            if (user == null)
+                return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                    return Unauthorized("Invalid password");
+            }
 
             return user;
         }
